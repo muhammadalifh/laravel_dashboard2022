21e40baa1d44fa78dce5bd498bba5aa73db89ae4
muhammadalifh <alifw50@gmail.com>
2022-02-10 11:23:18 +0700

    fix some stuff (create required)

--- resources/views/portofolio/create.blade.php
+++ resources/views/portofolio/create.blade.php
@@ -15,7 +15,7 @@
                         <div class="row mb-3">
                             <label for="klien" class="col-md-4 col-form-label text-md-end">{{ __('Klien') }}</label>
                             <div class="col-md-6">
-                                <select name="klien_id" id="klien_id" class="form-control select2">
+                                <select required name="klien_id" id="klien_id" class="form-control select2">
                                     <option disabled value selected>Pilih Klien</option>
                                     @foreach ($klien_create as $item)
                                             <option value="{{ $item->id }}">{{ $item->klien }}</option>
@@ -28,7 +28,7 @@
                             <label for="perusahaan" class="col-md-4 col-form-label text-md-end">{{ __('Perusahaan') }}</label>
 
                             <div class="col-md-6">
-                                <input id="perusahaan" type="text" class="form-control @error('perusahaan') is-invalid @enderror" name="perusahaan" value="{{ old('perusahaan') }}" required autocomplete="perusahaan" autofocus>
+                                <input required id="perusahaan" type="text" class="form-control @error('perusahaan') is-invalid @enderror" name="perusahaan" value="{{ old('perusahaan') }}" required autocomplete="perusahaan" autofocus>
 
                                 @error('perusahaan')
                                     <span class="invalid-feedback" role="alert">
@@ -42,7 +42,7 @@
                             <label for="tahun" class="col-md-4 col-form-label text-md-end">{{ __('Tahun') }}</label>
 
                             <div class="col-md-6">
-                                <input id="tahun" type="text" class="form-control @error('tahun') is-invalid @enderror" name="tahun" value="{{ old('tahun') }}" required autocomplete="tahun">
+                                <input required id="tahun" type="text" class="form-control @error('tahun') is-invalid @enderror" name="tahun" value="{{ old('tahun') }}" required autocomplete="tahun">
 
                                 @error('tahun')
                                     <span class="invalid-feedback" role="alert">
@@ -55,7 +55,7 @@
                         <div class="row mb-3">
                             <label for="jenis" class="col-md-4 col-form-label text-md-end">{{ __('Jenis (Project)') }}</label>
                             <div class="col-md-6">
-                                <select name="jenis_id" id="jenis_id" class="form-control select2">
+                                <select required name="jenis_id" id="jenis_id" class="form-control select2">
                                     <option disabled value selected>Pilih Jenis</option>
                                     @foreach ($jenis_create as $item)
                                             <option value="{{ $item->id }}">{{ $item->jenis }}</option>
@@ -67,7 +67,7 @@
                         <div class="row mb-3">
                             <label for="kapasitas" class="col-md-4 col-form-label text-md-end">{{ __('Kapasitas (m3/hari)') }}</label>
                             <div class="col-md-6">
-                                <input id="kapasitas" type="text" class="form-control @error('kapasitas') is-invalid @enderror" name="kapasitas" required autocomplete="kapasitas">
+                                <input required id="kapasitas" type="text" class="form-control @error('kapasitas') is-invalid @enderror" name="kapasitas" required autocomplete="kapasitas">
                                 @error('kapasitas')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -79,7 +79,7 @@
                         <div class="row mb-3">
                             <label for="teknologi" class="col-md-4 col-form-label text-md-end">{{ __('Teknologi') }}</label>
                             <div class="col-md-6">
-                                <select name="teknologi_id" id="teknologi_id" class="form-control select2">
+                                <select required name="teknologi_id" id="teknologi_id" class="form-control select2">
                                     <option disabled value selected>Pilih Teknologi</option>
                                     @foreach ($teknologi_create as $item)
                                             <option value="{{ $item->id }}">{{ $item->teknologi }}</option>
@@ -91,7 +91,7 @@
                         <div class="row mb-3">
                             <label for="nilai_kontrak" class="col-md-4 col-form-label text-md-end">{{ __('Nilai Kontrak') }}</label>
                             <div class="col-md-6">
-                                <input id="nilai_kontrak" type="text" class="form-control" name="nilai_kontrak" required autocomplete="nilai_kontrak">
+                                <input required id="nilai_kontrak" type="text" class="form-control" name="nilai_kontrak" required autocomplete="nilai_kontrak">
                                 @error('nilai_kontrak')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -103,7 +103,7 @@
                         <div class="row mb-3">
                             <label for="status" class="col-md-4 col-form-label text-md-end">{{ __('Status') }}</label>
                             <div class="col-md-6">
-                                <select name="status_id" id="status_id" class="form-control select2">
+                                <select required name="status_id" id="status_id" class="form-control select2">
                                     <option disabled value selected>Pilih Status</option>
                                     @foreach ($status_create as $item)
                                             <option value="{{ $item->id }}">{{ $item->status }}</option>
