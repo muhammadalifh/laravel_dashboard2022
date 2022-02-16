99364019361d3ca8fcfbc834fabc3520f94e27a1
muhammadalifh <alifw50@gmail.com>
2022-02-16 22:58:50 +0700

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>{{ config('app.name', 'Sistem Informasi Manajemen Penawaran Mitra Prima Enviro') }}</title>
+    <link rel="icon" href="{{ asset('images/logo/prima2.png') }}">
+    <title>Sistem Informasi Pelanggan Mitra Prima Enviro</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
